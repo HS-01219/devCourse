e67be0f762ef09eed9940196e8ffb9cccd13d8a0
HS <mcm009@naver.com>
2025-02-28 14:36:10 +0900

    feat : users 고도화
    유효성 검사 추가
    jwt를 이용해 토큰 발행 후 쿠키에 전송

--- week_5-1/routes/users.js
+++ week_5-1/routes/users.js
@@ -4,13 +4,35 @@
 const express = require("express");
 const router = express.Router();
 const conn = require("../mariadb");
+const {body, param, validationResult} = require("express-validator");
+// jwt
+const jwt = require("jsonwebtoken");
+
+// dotenv
+const dotenv = require("dotenv");
+dotenv.config();
 
 router.use(express.json());
 
-router.post("/login", (req, res) => {
-    const {email, pwd} = req.body;
+// 함수를 변수에 담으면서 모듈화
+const errorValidate = (req, res, next) => {
+    const error = validationResult(req);
+    if(error.isEmpty()) {
+        return next(); 
+    } else {
+        return res.status(400).json(error.array());
+    }
+}
 
-    if(email && pwd) {
+router.post("/login",
+    [
+        body('email').notEmpty().isEmail().withMessage('이메일 입력'),
+        body('pwd').notEmpty().isString().withMessage('문자입력'),
+        errorValidate
+    ], 
+    (req, res, next) => {
+        const {email, pwd} = req.body;
+
         // 해당 내용이 sql이라는 것을 알려주기 위해 변수를 명명해 사용
         // 꼭 중복 내용이 아니더라도 다른 사람에게 알려주는 용도로 변수를 사용할 수 있다.
         const sql = `SELECT * FROM users WHERE email = ?`;
@@ -28,32 +50,59 @@ router.post("/login", (req, res) => {
                 //     });
                 // }
 
+                if(err) {
+                    return res.status(500).json({ message : `조회 중 오류가 발생했습니다.` });
+                }
+
                 // 요즘 방식은 user 정보가 없을 경우 무조건 아이디 또는 비밀번호 확인 문구
                 if(loginUser && loginUser.password === pwd) {
+                    // 토큰 발급
+                    const token = jwt.sign({
+                        email : loginUser.email,
+                        name : loginUser.name
+                    }, process.env.PRIVATE_KEY, { 
+                        expiresIn : "1h",
+                        issuer : "HS"
+                    });
+                    // expiresIn : 토큰 만료 시간을 설정 (1h : 1시간)
+
+                    // 쿠키에 token이라는 공간을 만들어서 token을 저장
+                    
+                    // httpOnly : true 는 자바스크립트로 쿠키를 접근하지 못하게 하는 옵션
+                    // (= API 호출로만 접근 가능, XSS 공격 방지)
+
+                    // secure : true 는 https에서만 쿠키를 전송하겠다는 옵션
+                    // 요청 도메인이 https가 아니면 쿠키를 전송하지 않는다.
+
+                    // res 는 바로 cookie를 사용할 수 있으나, 
+                    // req에서는 cookie-parser (모듈)을 사용해야 한다.
+                    res.cookie("token", token, { httpOnly : true });
+
                     res.status(200).json({
                         message : `${loginUser.name}님, 환영합니다!`
                     });
                 } else {
-                    res.status(404).json({
+                    res.status(403).json({
                         message : "이메일 또는 비밀번호가 틀렸습니다."
                     });
                 }
             }
         );
-    } else {
-        res.status(400).json({
-            message : `입력 값을 다시 확인해주세요.`
-        });
-    }
 });
 
-router.post("/join", (req, res) => {
-    const {email, name, pwd, contact} = req.body;
-    
-    // req.body != {}, req.body.name !== "" 등의 방법은 속성이 누락되었을 때 검증할 수 없다.
-    // 값이 세개 다 존재할 때만 실행 (contact는 필수값이 아님)
-    // 화면이 존재한다면 거기서 먼저 return시켜주는 것이 좋을 것 같다.
-    if(email && name && pwd) {
+router.post("/join",
+    [
+        body('email').notEmpty().isEmail().withMessage('이메일 입력'),
+        body('name').notEmpty().isString().withMessage('이름 확인'),
+        body('pwd').notEmpty().isString().withMessage('비밀번호 확인'),
+        body('contact').isString().withMessage('연락처 확인'),
+        errorValidate
+    ], (req, res, next) => {
+        const {email, name, pwd, contact} = req.body;
+        
+        // req.body != {}, req.body.name !== "" 등의 방법은 속성이 누락되었을 때 검증할 수 없다.
+        // 값이 세개 다 존재할 때만 실행 (contact는 필수값이 아님)
+        // 화면이 존재한다면 거기서 먼저 return시켜주는 것이 좋을 것 같다.
         const sql = `INSERT INTO users (email, name, password, contact) VALUES (?, ?, ?, ?)`;
         const values = [email, name, pwd, contact];
         conn.query(
@@ -79,59 +128,66 @@ router.post("/join", (req, res) => {
                 }
             }
         );
-    } else {
-        res.status(400).json({
-            message : `입력 값을 다시 확인해주세요.`
-        });
-    }
 });
 
 // url이 중복될 경우 route를 이용하면 분기시켜줄 수 있다.
 router.route("/users")
-    .get((req, res) => {
-        let {email} = req.body;
-        
-        // 파라메터가 들어가는 부분을 ? 로 처리
-        // query 의 두번째 매개변수로 배열을 넣어주면 ?에 순서대로 들어간다. 
-        // 하나일 경우 email, [email] 둘 다 가능
+    .get(
+        [
+            param('email').notEmpty().isEmail().withMessage('이메일 입력'),
+            errorValidate
+        ],(req, res, next) => {
+            let {email} = req.body;
+            
+            // 파라메터가 들어가는 부분을 ? 로 처리
+            // query 의 두번째 매개변수로 배열을 넣어주면 ?에 순서대로 들어간다. 
+            // 하나일 경우 email, [email] 둘 다 가능
 
-        const sql = `SELECT * FROM users WHERE email = ?`;
-        conn.query(
-            sql, [email], function(err, results) {
-                if(results.length) {
-                    res.status(200).json(results);
-                } else {
-                    res.status(404).json({
-                        message : "회원 정보가 없습니다."
-                    });
-                }
-            }
-        );
-    })
-    .delete((req, res) => {
-        let {email} = req.body;
+            const sql = `SELECT * FROM users WHERE email = ?`;
+            conn.query(
+                sql, [email], function(err, results) {
+                    if(err) {
+                        return res.status(500).json({ message : `조회 중 오류가 발생했습니다.` });
+                    }
 
-        const sql = `DELETE FROM users WHERE email = ?`;
-        conn.query(
-            sql, [email], function(err, results) {
-                // DELETE 쿼리는 삭제된 row의 개수(affected rows)를 반환한다.
-                if(err) {
-                    res.status(500).json({
-                        message : "삭제 중 오류가 발생했습니다."
-                    });
-                } else {
-                    if(results.affectedRows === 0){
-                        res.status(404).json({
-                            message : "존재하지 않는 회원입니다."
-                        });
+                    if(results.length) {
+                        res.status(200).json(results);
                     } else {
-                        res.status(200).json({
-                            message : `삭제되었습니다. 감사합니다.`
+                        res.status(404).json({
+                            message : "회원 정보가 없습니다."
                         });
                     }
                 }
-            }
-        );
+            );
+    })
+    .delete(
+        [
+            param('email').notEmpty().isEmail().withMessage('이메일 입력'),
+            errorValidate
+        ], (req, res, next) => {
+            let {email} = req.body;
+
+            const sql = `DELETE FROM users WHERE email = ?`;
+            conn.query(
+                sql, [email], function(err, results) {
+                    // DELETE 쿼리는 삭제된 row의 개수(affected rows)를 반환한다.
+                    if(err) {
+                        res.status(500).json({
+                            message : "삭제 중 오류가 발생했습니다."
+                        });
+                    } else {
+                        if(results.affectedRows === 0){
+                            res.status(404).json({
+                                message : "존재하지 않는 회원입니다."
+                            });
+                        } else {
+                            res.status(200).json({
+                                message : `삭제되었습니다. 감사합니다.`
+                            });
+                        }
+                    }
+                }
+            );
     });
 
 /* 리팩토링 이후 안쓰는 함수
