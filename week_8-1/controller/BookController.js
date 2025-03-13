d061e7da10701d98e8c4000aa27e6684f77cb0c4
HS <mcm009@naver.com>
2025-03-13 15:20:21 +0900

    feat : 도서 API 페이징, 카테고리별/신간 조건 조회 추가

--- week_8-1/controller/BookController.js
+++ week_8-1/controller/BookController.js
@@ -1,39 +1,41 @@
 const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
+// (카테고리 별, 신간 여부) 전체 도서 목록 조회
 const allBooks = (req, res) => {
-    const {category_id} = req.query;
+    const { category_id, news, limit, currentPage } = req.query;
+    const offset = limit * (currentPage - 1);
+
+    let sql = `SELECT * FROM books WHERE 1 = 1`;
+    let values = [];
 
     if(category_id) {
-        const sql = `SELECT * FROM books WHERE category_id = ?`;
-        conn.query(sql, category_id, function(err, results) {
-            if(err) {
-                console.log(err);
-                return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message : `책 정보 조회 중 오류가 발생했습니다.` });
-            }
-
-            if(results.length) {
-                return res.status(StatusCodes.OK).json(results);
-            } else {
-                return res.status(StatusCodes.NOT_FOUND).end();
-            }
-        });
-    } else {
-        const sql = `SELECT * FROM books LEFT JOIN category ON books.category_id = category.id`;
-        conn.query(sql, function(err, results) {
-            if(err) {
-                console.log(err);
-                return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message : `책 정보 조회 중 오류가 발생했습니다.` });
-            }
-            return res.status(StatusCodes.OK).json(results);
-        });
+        sql += ` AND category_id = ?`;
+        values.push(category_id);
     }
+    
+    if (news) sql += ` AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`
+    
+    sql += ` LIMIT ? OFFSET ?`;
+    values.push(parseInt(limit), offset)
+    conn.query(sql, values, function(err, results) {
+        if(err) {
+            console.log(err);
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message : `책 정보 조회 중 오류가 발생했습니다.` });
+        }
+
+        if(results.length) {
+            return res.status(StatusCodes.OK).json(results);
+        } else {
+            return res.status(StatusCodes.NOT_FOUND).end();
+        }
+    });
 }
 
 const bookDetail = (req, res) => {
     const id = parseInt(req.params.id);
 
-    const sql = `SELECT * FROM books LEFT JOIN category ON books.category_id = category.id WHERE id = ?`;
+    const sql = `SELECT * FROM books LEFT JOIN category ON books.category_id = category.id WHERE books.id = ?`;
     conn.query(sql, id, function(err, results) {
         if(err) {
             console.log(err);
