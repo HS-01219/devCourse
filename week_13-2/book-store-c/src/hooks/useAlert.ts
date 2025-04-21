6cf33348910a5be8ab993c42489b734ea8b94c2c
HS <mcm009@naver.com>
2025-04-21 20:03:12 +0900

    feat : window.alert 호출 hook

---
+++ week_13-2/book-store-c/src/hooks/useAlert.ts
@@ -0,0 +1,11 @@
+import { useCallback } from "react"
+
+// 추후 디자인 또는 라이브러리를 적용할 때 해당 파일만 수정하면 되도록
+// hook을 만들어서 관리 
+export const useAlert = () => {
+    const showAlert = useCallback((message : string) => {
+        window.alert(message);
+    }, []);
+
+    return showAlert;
+}
